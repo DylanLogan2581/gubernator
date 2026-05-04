bc113f5c78a4f01709a76b277baba28972804d4a
Dylan Logan <github@dylanlogan.xyz>
2026-05-03 20:28:39 -0400

    feat(turns): add epic 2 feature entrypoint exports
    Export the implemented Epic #2 modules through public feature entrypoints.
    
    fix #105

--- src/features/notifications/index.ts
+++ src/features/notifications/index.ts
@@ -2,5 +2,6 @@ export {
   turnCompletedNotificationsQueryOptions,
   unreadNotificationsCountQueryOptions,
   type TurnCompletedNotification,
+  type TurnCompletedNotificationsFilters,
 } from "./queries/notificationQueries";
 export { notificationQueryKeys } from "./queries/notificationQueryKeys";
